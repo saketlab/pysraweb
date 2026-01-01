9b44268b9540ca9e1c069b909cadc0fa9a1764c1
amkhrjee <aniruddha.mukherji@outlook.com>
2026-01-01 23:51:58 +0530

    refactor: simplify HomeSearchBar component by removing unused search methods and filters

--- components/home-search-bar.tsx
+++ components/home-search-bar.tsx
@@ -1,20 +1,6 @@
 "use client";
-import {
-  EyeClosedIcon,
-  EyeOpenIcon,
-  MagicWandIcon,
-  MagnifyingGlassIcon,
-  MixerHorizontalIcon,
-} from "@radix-ui/react-icons";
-import {
-  Box,
-  Button,
-  Card,
-  Flex,
-  Grid,
-  Select,
-  TextField,
-} from "@radix-ui/themes";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { Box, Flex, Text, TextField } from "@radix-ui/themes";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
@@ -27,8 +13,6 @@ enum searchType {
 export default function HomeSearchBar() {
   const [query, setQuery] = useState("");
   const router = useRouter();
-  const [searchMethod, setSearchMethod] = useState(searchType.NLPSearch);
-  const [hideFilters, setHideFilters] = useState(true);
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
@@ -45,7 +29,7 @@ export default function HomeSearchBar() {
       gap="4"
       mt={{ initial: "4rem" }}
     >
-      <Box width={"10rem"}>
+      <Box width={"12rem"}>
         <Image
           src="/pysradb_v3.png"
           alt="pysradb logo"
@@ -59,133 +43,22 @@ export default function HomeSearchBar() {
           unoptimized
         />
       </Box>
+      <Text weight={"medium"} color="gray">
+        Discover GEO & SRA datasets with natural language
+      </Text>
       <Box width={{ initial: "85%", md: "60%" }}>
         <Flex direction={"column"} gap={"4"}>
-          {searchMethod == searchType.NLPSearch && (
-            <form onSubmit={handleSubmit}>
-              <TextField.Root
-                placeholder="Start typing..."
-                size="3"
-                onChange={(e) => setQuery(e.target.value)}
-              >
-                <TextField.Slot>
-                  <MagnifyingGlassIcon height="16" width="16" />
-                </TextField.Slot>
-              </TextField.Root>
-            </form>
-          )}
-          {searchMethod == searchType.FilterSearch && (
-            <Card variant="surface">
-              <form
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "1rem",
-                }}
-              >
-                <Grid columns={"2"} gap={"2"}>
-                  <label>Scientific name</label>
-                  <TextField.Root placeholder="homo sapiens" />
-
-                  <label>Technology</label>
-                  <TextField.Root placeholder="Illumina" />
-
-                  <label>Tissue/organ</label>
-                  <TextField.Root placeholder="blood" />
-
-                  <label>Cell type</label>
-                  <TextField.Root placeholder="CD8+" />
-
-                  <label>Disease</label>
-                  <TextField.Root placeholder="Diabetes" />
-
-                  {!hideFilters && (
-                    <>
-                      <label>Phenotype</label>
-                      <TextField.Root placeholder="" />
-
-                      <label>Disease Stage</label>
-                      <TextField.Root placeholder="" />
-
-                      <label>Strain</label>
-                      <TextField.Root placeholder="" />
-
-                      <label>Treatment</label>
-                      <TextField.Root placeholder="" />
-
-                      <label>Ethnicity</label>
-                      <TextField.Root placeholder="asian" />
-
-                      <label>Sex</label>
-                      <Select.Root defaultValue="both">
-                        <Select.Trigger />
-                        <Select.Content>
-                          <Select.Group>
-                            <Select.Label>Select sex</Select.Label>
-                            <Select.Item value="male">Male</Select.Item>
-                            <Select.Item value="female">Female</Select.Item>
-                            <Select.Item value="both">
-                              Male & Female
-                            </Select.Item>
-                          </Select.Group>
-                        </Select.Content>
-                      </Select.Root>
-
-                      <label>Oldest publication date</label>
-                      <TextField.Root type="date" />
-                    </>
-                  )}
-                </Grid>
-              </form>
-            </Card>
-          )}
-          {searchMethod == searchType.FilterSearch && (
-            <Flex justify={"center"} gap={"4"}>
-              <Button
-                onClick={() => setHideFilters(!hideFilters)}
-                style={{ width: "max-content" }}
-                variant="soft"
-              >
-                {hideFilters ? (
-                  <>
-                    {" "}
-                    <EyeOpenIcon /> Show more filters
-                  </>
-                ) : (
-                  <>
-                    {" "}
-                    <EyeClosedIcon /> Hide filters
-                  </>
-                )}
-              </Button>
-              <Button>
-                <MagnifyingGlassIcon /> Search
-              </Button>
-            </Flex>
-          )}
-          <Button
-            onClick={() =>
-              setSearchMethod(
-                searchMethod == searchType.NLPSearch
-                  ? searchType.FilterSearch
-                  : searchType.NLPSearch
-              )
-            }
-            style={{ width: "max-content", margin: "auto" }}
-            variant="ghost"
-          >
-            {searchMethod == searchType.NLPSearch ? (
-              <>
-                {" "}
-                <MixerHorizontalIcon /> Search with filters
-              </>
-            ) : (
-              <>
-                {" "}
-                <MagicWandIcon /> Search in natural language
-              </>
-            )}
-          </Button>
+          <form onSubmit={handleSubmit}>
+            <TextField.Root
+              size="3"
+              onChange={(e) => setQuery(e.target.value)}
+              autoFocus
+            >
+              <TextField.Slot>
+                <MagnifyingGlassIcon height="16" width="16" />
+              </TextField.Slot>
+            </TextField.Root>
+          </form>
         </Flex>
       </Box>
     </Flex>
