f92d8e201d2390f40244721ce53e577397aaaee0
Saket Choudhary <saketc@iitb.ac.in>
2026-01-17 15:12:30 +0530

    Add github workflow

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,2 @@
-export const SERVER_URL = "http://192.168.68.170:8000";
-// export const SERVER_URL = "http://10.61.189.20:8000";
-// export const SERVER_URL = "http://localhost:8000";
-// export const SERVER_URL = "http://10.61.4.52:8000";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
