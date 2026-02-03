34a94af75d17dda428fb282be05a376942532b9f
amkhrjee <aniruddha.mukherji@outlook.com>
2026-02-03 12:28:24 +0530

    fix: restore constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,5 @@
-const SERVER_URL =process.env.NEXT_PUBLIC_API_URL ?? "https://pysraweb.saketlab.org/api";
-  
+let SERVER_URL = "/api";
+if (process.env.NEXT_PUBLIC_ENVIRONMENT === "DEV") {
+  SERVER_URL = "https://pysraweb.saketlab.org/api";
+}
 export { SERVER_URL };
-
-// checks if SERVER_URL is valid URL, im using a check here in single line - to reduce the redundancy of creating .env file again and again. 
