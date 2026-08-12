ca7db96f9f8315cb9b6697ae0f28712911ff1816
Aniruddha Mukherjee <aniruddha.mukherji@outlook.com>
2026-08-13 04:30:56 +0530

    fix: improve wording in navbar for cli

--- utils/nav.ts
+++ utils/nav.ts
@@ -32,7 +32,7 @@ export const NAV_ITEMS: readonly NavItem[] = [
     icon: createElement(InfoCircledIcon),
   },
   {
-    label: "CLI & Libraries",
+    label: "CLI & Packages",
     href: "/cli",
     external: true,
     icon: createElement(KeyboardIcon),
